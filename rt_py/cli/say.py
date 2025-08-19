512283f9a1e1a30a82a92deddc2a3dab6a305b8b
Ivo Bellin Salarin <ivo@nilleb.com>
2025-08-19 09:43:20 +0200

    feat: say - explore text to speech

---
+++ rt_py/cli/say.py
@@ -0,0 +1,33 @@
+import signal
+import sys
+
+import sounddevice as sd
+from rt_py.bricks.tts import get_tts_engine
+
+tts = get_tts_engine()
+running = True
+
+
+def handle_sigint(sig, frame):
+    global running
+    running = False
+
+
+signal.signal(signal.SIGINT, handle_sigint)
+
+if len(sys.argv) > 1:
+    text = " ".join(sys.argv[1:])
+    samples, sample_rate = tts.create(text, voice="af_heart")
+    sd.play(samples, sample_rate)
+    sd.wait()
+    exit(0)
+
+print("Press Ctrl+D or enter an empty line to exit.")
+while running:
+    text = sys.stdin.readline().strip()
+    if not text:
+        break
+
+    samples, sample_rate = tts.create(text, voice="af_heart")
+    sd.play(samples, sample_rate)
+    sd.wait()
